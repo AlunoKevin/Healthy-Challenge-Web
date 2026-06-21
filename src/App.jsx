68a63be3e22cad9ec0bc5920f8adedad655c458b
ArthurNorb <arthurnorberto4@gmail.com>
2026-06-21 13:37:20 -0300

    Revert "fluxo de login para ranking"
    This reverts commit 4caf2cb903f16aa70ad5b08752376d68bea0709a.

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,29 @@ import Cadastro from './components/Cadastro';
 import Ranking from './components/Ranking';
 
 function App() {
-  const [telaAtual, setTelaAtual] = useState('login');
+  const [telaAtual, setTelaAtual] = useState('ranking');
 
   return (
     <div style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
+      
       {telaAtual === 'login' && (
         <Login 
           onIrParaCadastro={() => setTelaAtual('cadastro')} 
-          onSucesso={() => setTelaAtual('ranking')}
+          onSucesso={() => setTelaAtual('ranking')} 
         />
       )}
+
       {telaAtual === 'cadastro' && (
         <Cadastro 
           onIrParaLogin={() => setTelaAtual('login')} 
-          onSucesso={() => setTelaAtual('ranking')}
+          onSucesso={() => setTelaAtual('ranking')} 
         />
       )}
+
       {telaAtual === 'ranking' && (
         <Ranking />
       )}
+      
     </div>
   );
 }
