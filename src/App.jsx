4caf2cb903f16aa70ad5b08752376d68bea0709a
taylanne <taylanne.mendes@gmail.com>
2026-06-21 01:31:02 -0300

    fluxo de login para ranking

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,25 @@ import Cadastro from './components/Cadastro';
 import Ranking from './components/Ranking';
 
 function App() {
-  const [telaAtual, setTelaAtual] = useState('ranking');
+  const [telaAtual, setTelaAtual] = useState('login');
 
   return (
     <div style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
-      
       {telaAtual === 'login' && (
         <Login 
           onIrParaCadastro={() => setTelaAtual('cadastro')} 
-          onSucesso={() => setTelaAtual('ranking')} 
+          onSucesso={() => setTelaAtual('ranking')}
         />
       )}
-
       {telaAtual === 'cadastro' && (
         <Cadastro 
           onIrParaLogin={() => setTelaAtual('login')} 
-          onSucesso={() => setTelaAtual('ranking')} 
+          onSucesso={() => setTelaAtual('ranking')}
         />
       )}
-
       {telaAtual === 'ranking' && (
         <Ranking />
       )}
-      
     </div>
   );
 }
